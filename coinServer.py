ce85956be5f331dc4fcdfe349e39ee5ebc3bf1cd
Gabriel Arruda <gabrielpardi@outlook.com>
2018-10-09 05:27:05 -0300

    mudança de ip

--- coinServer.py
+++ coinServer.py
@@ -1,10 +1,8 @@
 from socket import *
 
 file = open("peers.txt", "w+")
-
-serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('127.0.0.1', serverPort))
+serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 
 print("The server is ready")
